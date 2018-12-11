3b6c9536202f89f226fc8c0a3030f408838aa007
Andreas Grillenberger <andreas.grillenberger@fu-berlin.de>
2018-12-11 08:37:58 +0100

    added request manager

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const request = require("request");
+const md5 = require("md5");
+
+var runningRequests = {};
 
 exports.request = function(url, callback) {
-  request(url).on('data', function(data) {
+  var r = request(url).on('data', function(data) {
     var myData = data.toString(); // convert to string
     var firstNL = myData.indexOf("\n")
     var expLength = parseInt(myData.substring(0, firstNL - 1), 16) + 2; // +2 for \r\n
@@ -14,5 +17,18 @@ exports.request = function(url, callback) {
     } catch (e) { }
 
     callback(ret);
-  })
+  });
+
+  runningRequests[md5(url)] = r;
+
+  return md5(url);
 };
+
+exports.abort = function(hash) {
+  if(runningRequests[hash]) {
+    runningRequests[hash].abort();
+    return true;
+  }
+
+  return false;
+}
